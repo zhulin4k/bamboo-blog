b6aabcac915c438006d81c5e2bf180a4382e3d03
zhulin4kai <2720460362@qq.com>
2024-11-23 22:59:52 +0800

    demo builded

---
+++ src/main/java/com/artemis/blog/ArtemisBlogApplication.java
@@ -0,0 +1,13 @@
+package com.artemis.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtemisBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtemisBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/artemis/blog/ArtemisBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artemis.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtemisBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
